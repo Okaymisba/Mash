6377576f7e6b46ec5edb05e6b89493564e97b2e2
okaymisba <msbahsarfaraz@gmail.com>
2025-01-25 20:05:56 +0500

    Updated the main.cpp which was giving error regarding parser.

--- main.cpp
+++ main.cpp
@@ -5,20 +5,46 @@
 
 using namespace std;
 
+void printAST(const ASTNode &node, int indent = 0)
+{
+    for (int i = 0; i < indent; ++i)
+        cout << "  ";
+    cout << node.type;
+    if (!node.value.empty())
+        cout << " (" << node.value << ")";
+    cout << endl;
+    for (const auto &child : node.children)
+    {
+        printAST(child, indent + 1);
+    }
+}
+
 int main()
 {
-    string code = R"( x= 'M';)";
+    string code = R"( x= 'M')";
 
     Tokenizer tokenizer;
     Evaluator evaluator;
-    Parser parser;
 
-    vector<token> tokens = tokenizer.tokenize(code);
+    vector<Token> tokens = tokenizer.tokenize(code);
 
     for (const auto &token : tokens)
     {
         cout << "Token Type: " << token.type << ", Token Value: " << token.value << endl;
     }
+
+    cout << "\nIgnore bellow this: " << endl;
+    try
+    {
+        Parser parser(tokens);
+        ASTNode ast = parser.parse();
+        printAST(ast);
+    }
+    catch (const runtime_error &e)
+    {
+        cerr << "Error: " << e.what() << endl;
+    }
+    cout << "Uptill here" << endl;
 
     return 0;
 };
