170b2ae6b1395bc1cbd6ada9bb3d984ba7fb8f4d
okaymisba <msbahsarfaraz@gmail.com>
2025-02-01 13:08:55 +0500

    Updated Parser.cpp to handle nested expressions and expressions inside print statement.
    Now for code:
    x = 5 + 1;
    y = 6 * (1 + 3);
    print( "Sum is: " + (x+y))
    
    The AST will look like this:
    PROGRAM
      ASSIGNMENT
        IDENTIFIER (x)
        NUMBER (5)
        ARITHMETIC_OPERATOR (+)
        NUMBER (1)
      ASSIGNMENT
        IDENTIFIER (y)
        NUMBER (6)
        ARITHMETIC_OPERATOR (*)
        EXPRESSION
          NUMBER (1)
          ARITHMETIC_OPERATOR (+)
          NUMBER (3)
      PRINT
        STRING ("Sum is: ")
        ARITHMETIC_OPERATOR (+)
        EXPRESSION
          IDENTIFIER (x)
          ARITHMETIC_OPERATOR (+)
          IDENTIFIER (y)

--- Parser.cpp
+++ Parser.cpp
@@ -45,12 +45,34 @@ Token Parser::consume(const string &expectedType)
 ASTNode Parser::parseExpression()
 {
     Token token = peek();
-    if (token.type == "IDENTIFIER" || token.type == "NUMBER" || token.type == "ARITHMETIC_OPERATOR" || token.type == "STRING" || token.type == "CHAR")
+
+    if (token.type == "OPEN_ROUND_BRACKET")
+    {
+        if (token.type == "OPEN_ROUND_BRACKET")
+        {
+            consume("OPEN_ROUND_BRACKET");
+        }
+
+        ASTNode literal("EXPRESSION");
+
+        while (peek().type != "CLOSE_ROUND_BRACKET")
+        {
+            ASTNode expression = parseExpression();
+            literal.children.push_back(expression);
+        }
+
+        consume("CLOSE_ROUND_BRACKET");
+
+        return literal;
+    }
+
+    else if (token.type == "IDENTIFIER" || token.type == "NUMBER" || token.type == "ARITHMETIC_OPERATOR" || token.type == "STRING" || token.type == "CHAR")
     {
         consume(token.type);
         ASTNode node(token.type, token.value);
         return node;
     }
+
     else
     {
         throw runtime_error("Syntax error: Unexpected Token: '" + token.type + "'");
@@ -74,7 +96,6 @@ ASTNode Parser::parseAssignment()
     return assignment;
 }
 
-// TODO: Handling of expressions in print
 ASTNode Parser::parsePrintStatement()
 {
     consume("PRINT");
