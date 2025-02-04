1c6e5242ca73330c8ee866122d4bee0afac077c9
okaymisba <msbahsarfaraz@gmail.com>
2025-02-04 12:04:32 +0500

    Shifted parseExpression function to func/parseExpression/parseExpression.cpp

--- Parser.cpp
+++ Parser.cpp
@@ -49,43 +49,6 @@ Token Parser::consume(const string &expectedType)
     }
 }
 
-ASTNode Parser::parseExpression()
-{
-    Token token = peek();
-
-    if (token.type == "OPEN_ROUND_BRACKET")
-    {
-        if (token.type == "OPEN_ROUND_BRACKET")
-        {
-            consume("OPEN_ROUND_BRACKET");
-        }
-
-        ASTNode literal("EXPRESSION");
-
-        while (peek().type != "CLOSE_ROUND_BRACKET")
-        {
-            ASTNode expression = parseExpression();
-            literal.children.push_back(expression);
-        }
-
-        consume("CLOSE_ROUND_BRACKET");
-
-        return literal;
-    }
-
-    else if (token.type == "IDENTIFIER" || token.type == "NUMBER" || token.type == "ARITHMETIC_OPERATOR" || token.type == "STRING" || token.type == "CHAR" || token.type == "BOOL")
-    {
-        consume(token.type);
-        ASTNode node(token.type, token.value);
-        return node;
-    }
-
-    else
-    {
-        throw runtime_error("Syntax error: Unexpected Token: '" + token.type + "'");
-    }
-}
-
 ASTNode Parser::parseAssignment()
 {
     Token identifier = consume("IDENTIFIER");
