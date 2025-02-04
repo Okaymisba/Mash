5786b88b7e4ab130bab8b1d933548c0e56e51c5f
okaymisba <msbahsarfaraz@gmail.com>
2025-02-04 12:13:03 +0500

    Shifted consume function to func/cinsume/consume.cpp

--- Parser.cpp
+++ Parser.cpp
@@ -26,20 +26,6 @@ ASTNode Parser::parse()
     return root;
 };
 
-Token Parser::consume(const string &expectedType)
-{
-    Token token = peek();
-    if (token.type == expectedType)
-    {
-        currentIndex++;
-        return token;
-    }
-    else
-    {
-        throw runtime_error("Unexpected token: expected " + expectedType + ", got " + token.type);
-    }
-}
-
 ASTNode Parser::parseAssignment()
 {
     Token identifier = consume("IDENTIFIER");
