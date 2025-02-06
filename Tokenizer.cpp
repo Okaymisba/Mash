f50841c7b954d69f0ca47aa13b057b3006a35a63
Ali2006-NED <aliahmed23022006@gmail.com>
2025-02-06 22:45:16 +0500

    Formatted source code

--- Tokenizer.cpp
+++ Tokenizer.cpp
@@ -64,12 +64,14 @@ vector<Token> Tokenizer::tokenize(string &input)
 
                 if (key != "WHITESPACE")
                 {
-                    if(key == "STRING" || key == "CHAR"){
+                    if (key == "STRING" || key == "CHAR")
+                    {
                         string str = match[i + 1];
-                        string string_text = str.substr(1,str.length()-2);
-                        tokens.emplace_back(key,string_text);
+                        string string_text = str.substr(1, str.length() - 2);
+                        tokens.emplace_back(key, string_text);
                     }
-                    else{
+                    else
+                    {
                         tokens.emplace_back(key, match[i + 1].str());
                     }
                 }
