014882aa99b4ba6f50bce2f371f026646a7b0013
okaymisba <msbahsarfaraz@gmail.com>
2025-02-08 10:46:50 +0500

    Added comment for parseRight()

--- func/parseRight/parseRight.cpp
+++ func/parseRight/parseRight.cpp
@@ -1,5 +1,14 @@
 #include "../../Parser.h"
 
+/**
+ * Parses the right operand of an expression.
+ *
+ * If the next token is an open round bracket, this function parses an expression
+ * enclosed in parentheses. Otherwise, it parses a term.
+ *
+ * @return An Abstract Syntax Tree node representing the parsed right operand.
+ */
+
 ASTNode Parser::parseRight()
 {
     if (peek().type == "OPEN_ROUND_BRACKET")
