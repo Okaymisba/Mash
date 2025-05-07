3e98e115d0a0250343ced2973d24e4b6278f0ed0
Ali2006-NED <aliahmed23022006@gmail.com>
2025-05-07 20:31:40 +0500

    Refactor: Move RETURN parsing into the main loop of parseBody

--- func/parseBody/parseBody.cpp
+++ func/parseBody/parseBody.cpp
@@ -12,43 +12,44 @@
  * the constructed BODY node.
  */
 
-ASTNode Parser::parseBody()
-{
-    while (peek().type == "LINE_BREAK")
-    {
-        consume("LINE_BREAK");
-    }
-
-    consume("OPEN_CURLY_BRACKET");
-    ASTNode body("BODY");
-
-    while (peek().type != "CLOSE_CURLY_BRACKET")
-    {
-        if (peek().type == "LINE_BREAK")
-        {
-            consume("LINE_BREAK");
-        }
-        else
-        {
-            ASTNode statement = parseStatement();
-            body.children.push_back(statement);
-        }
-    }
-    if (peek().type == "RETURN")
-    {
-        consume("RETURN");
-        ASTNode Return("RETURN");
-        ASTNode returnExpression = parseExpression();
-        Return.children.push_back(returnExpression);
-        body.children.push_back(Return);
-    }
-
-    consume("CLOSE_CURLY_BRACKET");
-
-    while (peek().type == "LINE_BREAK")
-    {
-        consume("LINE_BREAK");
-    }
-
-    return body;
-}
+ ASTNode Parser::parseBody()
+ {
+     while (peek().type == "LINE_BREAK")
+     {
+         consume("LINE_BREAK");
+     }
+ 
+     consume("OPEN_CURLY_BRACKET");
+     ASTNode body("BODY");
+ 
+     while (peek().type != "CLOSE_CURLY_BRACKET")
+     {
+         if (peek().type == "LINE_BREAK")
+         {
+             consume("LINE_BREAK");
+         }
+         else if (peek().type == "RETURN")
+         {
+             consume("RETURN");
+             ASTNode Return("RETURN");
+             ASTNode returnExpression = parseExpression();
+             Return.children.push_back(returnExpression);
+             body.children.push_back(Return);
+         }
+         else
+         {
+             ASTNode statement = parseStatement();
+             body.children.push_back(statement);
+         }
+     }
+ 
+     consume("CLOSE_CURLY_BRACKET");
+ 
+     while (peek().type == "LINE_BREAK")
+     {
+         consume("LINE_BREAK");
+     }
+ 
+     return body;
+ }
+ 
